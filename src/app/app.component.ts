bd286a2b8abeaa4051fa776a5748554427b063e5
Lucas Sebe <lucassebe16@gmail.com>
2024-08-08 15:27:01 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderTopComponent } from "./components/header-top/header-top.component";
+import { HeaderSectionComponent } from "./components/header-section/header-section.component";
+import { OurServicesComponent } from "./components/our-services/our-services.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { IframeVideoComponent } from "./components/iframe-video/iframe-video.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderTopComponent, HeaderSectionComponent, OurServicesComponent, AboutUsComponent, IframeVideoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/iframe-video/iframe-video.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iframe-video',
+  standalone: true,
+  imports: [],
+  templateUrl: './iframe-video.component.html',
+  styleUrl: './iframe-video.component.scss'
+})
+export class IframeVideoComponent {
+
+}
