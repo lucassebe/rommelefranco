b4632a694e7bd1e3b1907b59092b5e9cfbb7249b
Lucas Sebe <lucassebe16@gmail.com>
2024-08-13 16:49:44 -0300

    alteracoes mobile

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -16,21 +16,21 @@ export class ReviewsComponent implements OnInit {
       date: '05/08/2024',
       rating: 5,
       comment: 'Nossa Construtora sempre prezou pela segurança jurídica dos nossos documentos e dos vínculos com os nossos clientes e essa excelência é garantida pelo Rommel e Franco Advogados... sempre extremamente técnicos e éticos! Hoje, podemos dizer que somos bem assessorados, principalmente na área contratual!',
-      image: 'https://via.placeholder.com/40' 
+      image: 'construpat.jpeg' 
     },
     {
       name: 'Aluisio Silva',
       date: '05/08/2024',
       rating: 5,
       comment: 'Atendimento formidável, ambiente acolhedor e profissionais exemplares. São as melhores definições para qualificar o trabalho de profissionais da envergadura do Dr. Vladmyr e da Dra. Bianca! Gratidão por todo o suporte jurídico prestado à nossa empresa. Sucesso.',
-      image: 'https://via.placeholder.com/40' 
+      image: 'aluisio.png' 
     },
     {
       name: 'Denyze Moraes',
       date: '05/08/2024',
       rating: 5,
       comment: 'Tivemos uma excelente experiência quando necessitamos dos serviços profissionais desta firma de advogados. Atenciosos, comprometidos e muito qualificados, nos deram o apoio e a solução do problema que precisávamos com transparência e agilidade. Recomendo.',
-      image: 'https://via.placeholder.com/40' 
+      image: 'denyze.png' 
     }
     ,
     {
@@ -38,42 +38,42 @@ export class ReviewsComponent implements OnInit {
       date: '05/08/2024',
       rating: 5,
       comment: 'Precisei entrar com um processo sobre vários móveis que não tinha recebido. Já tinha tentado de tudo e não estava conseguindo. O pessoal do escritório recebeu minha demanda, sempre atentos, solícitos e muito transparentes. Muito profissionais e éticos. Recomendo demais!',
-      image: 'https://via.placeholder.com/40' 
+      image: 'marilia.png' 
     },
     {
       name: 'Nauan Medeiros',
       date: '05/08/2024',
       rating: 5,
       comment: 'Excelentes profissionais de uma competência inigualável! Muito satisfeita com seus serviços.',
-      image: 'https://via.placeholder.com/40' 
+      image: 'nauan.jpeg' 
     },
     {
       name: 'Valéria Marques',
       date: '05/08/2024',
       rating: 5,
       comment: 'Obrigada pelo excelente atendimento! Vocês são maravilhosos.',
-      image: 'https://via.placeholder.com/40' 
+      image: 'valeria.png' 
     },
     {
       name: 'Matheus Chaves',
       date: '05/08/2024',
       rating: 5,
       comment: 'Excelente equipe. Espaço acolhedor e confortável',
-      image: 'https://via.placeholder.com/40' 
+      image: 'matheus.png' 
     },
     {
       name: 'André Fonteles',
       date: '11/08/2024',
       rating: 5,
       comment: 'Extremamente satisfeito com o atendimento e profissionalismo do escritório. Os profissionais são altamente qualificados e dedicados, sempre prontos para esclarecer diversas dúvidas e indicar as melhores soluções. Recomendo a todas as empresas do setor de construção civil!!',
-      image: 'https://via.placeholder.com/40' 
+      image: 'andre.jpeg' 
     },
     {
       name: 'Lucas Sebe',
       date: '12/08/2024',
       rating: 5,
       comment: 'O compromisso com a ética e a dedicação ao cliente são evidentes em cada interação. Recomendo fortemente!',
-      image: 'https://via.placeholder.com/40' 
+      image: 'lucas.png' 
     },
   ];
   
